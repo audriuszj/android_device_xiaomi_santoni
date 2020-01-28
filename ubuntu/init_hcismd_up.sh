be66a535405fd59cb7fc9b9136e050c297dcae5c
Danct12 <danct12@disroot.org>
2020-01-28 23:55:20 +0700

    ubuntu/init_hcismd_up: modified a bit

--- ubuntu/init_hcismd_up.sh
+++ ubuntu/init_hcismd_up.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-## From bitrvmpd's SailfishOS port
-## Modified by asriel-danctnix (for ubports-santoni)
-
-# Maximum number of attempts to enable hcismd to try to get
+#Maximum number of attempts to enable hcismd to try to get
 # hci0 to come online.  Writing to sysfs too early seems to
 # not work, so we loop.
 MAXTRIES=15
+
+export LD_LIBRARY_PATH=/vendor/lib64:/system/lib64:/vendor/lib:/system/lib
+
+#Wifi enabler
+j=1
+while [ ! $j -gt $MAXTRIES ]  ; do
+    echo 1 > /dev/wcnss_wlan
+    echo sta > /sys/module/wlan/parameters/fwpath
+    
+    if [ "$?" -ne "0" ]; then
+      sleep 1
+    fi
+    
+    j=$((j + 1))
+done
 
 #setprop bluetooth.hciattach true
 setprop ro.qualcomm.bt.hci_transport smd
@@ -15,24 +27,24 @@ setprop qcom.bt.le_dev_pwr_class 2
 
 i=1
 while [ ! $i -gt $MAXTRIES ]  ; do
-    # set wcnss fwpath first
-    echo sta > /sys/module/wlan/parameters/fwpath
     echo 1 > /sys/module/hci_smd/parameters/hcismd_set
     if [ -e /sys/class/bluetooth/hci0 ] ; then
-        # found hci0, exit successfully
+        # found hci0, get/set BT MAC address
         echo 0 > /sys/module/hci_smd/parameters/hcismd_set
-        bt_mac=$(/system/bin/hci_qcomm_init -e -p 2 -P 2 -d /dev/ttyHSL0 2>&1 | grep -oP '([0-9a-f]{2}:){5}([0-9a-f]{2})')
+        bt_mac=$(/system/bin/hci_qcomm_init -e -p 2 -P 2 -d /dev/ttyHSL0 2>1 | grep -oE '([0-9a-f]{2}:){5}([0-9a-f]{2})')
         echo "BT MAC: $bt_mac"
-        if [ -n "$bt_mac" ] ; then
-            echo "$bt_mac" > /var/lib/bluetooth/board-address
+        if [ ! -z "$bt_mac" ] ; then
+            echo $bt_mac > /var/lib/bluetooth/board-address
             echo "BT MAC: $bt_mac"
         fi
         echo 1 > /sys/module/hci_smd/parameters/hcismd_set
         exit 0
     fi
     sleep 1
-    if [ $i = $MAXTRIES ] ; then
+    i=$((i + 1))
+    if [ $i == $MAXTRIES ] ; then
         # must have gotten through all our retries, fail
         exit 1
     fi
+
 done
